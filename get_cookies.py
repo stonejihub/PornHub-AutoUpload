5e2cd4caf6d781d504f0b186ef97897694e73aca
StoneHub <69335072+stonejihub@users.noreply.github.com>
2020-09-27 12:08:24 -0400

    Update get_cookies.py
    delete unnecessary comments

--- get_cookies.py
+++ get_cookies.py
@@ -34,53 +34,7 @@ chrome.find_element_by_xpath("//input[@id='password']").send_keys(password)
 element = WebDriverWait(chrome,10).until(EC.element_to_be_clickable((By.ID, "submit")))
 element.click()
 
+# sleep to sleep you have login
 time.sleep(5)
 save_cookies(chrome, cookies_location)
 chrome.quit()
-
-# Click login button
-# chrome.find_element_by_css_selector('#main-content #submit').click()
-# chrome.find_element_by_css_selector('#submit').click()
-# chrome.find_element_by_xpath("//form[@id='signup_form']/section/input[3]").click()
-# chrome.find_element_by_xpath("//input[@type='submit']").click()
-# chrome.find_element_by_xpath("//input[@id='submit']").click()
-# chrome.find_element_by_xpath("//input[@id='submit'][@type='submit']").click()
-# chrome.find_element_by_xpath(
-#     "//input[contains(@class,'buttonBase create_account_button_disabled')]").submit()
-# chrome.find_element_by_xpath(
-# "//form[@id='signup_form']/section[@class='formStepOne']/input[@id='submit']").click()
-# chrome.find_element_by_id('submit').click()
-# chrome.find_element_by_xpath("/html/body/div[6]/div/div[3]/section/div/div[2]/form/section/input[3]").click()
-# login_button = chrome.find_element_by_xpath("//input[@id='submit'and @type='submit']")
-# login_button = chrome.find_element_by_xpath("//input[@id='submit'and @type='submit' and @class='buttonBase create_account_button_disabled big orangeButton light js-loginSubmit']")
-# login_button = chrome.find_element_by_class_name('buttonBase create_account_button_disabled big orangeButton light js-loginSubmit')
-# print(login_button)
-# login_button.click()
-# chrome.find_element_by_xpath("//input[@id='submit']").send_keys('\n')
-# chrome.find_element_by_xpath("//input[@id='submit']").submit()
-# chrome.find_element_by_xpath("//input[@id='submit']").send_keys('Keys.ENTER')
-# chrome.find_element_by_id("submit").click()
-
-# Load of the page you cant access without cookies, this one will fail
-# chrome = webdriver.Chrome('./chromedriver')
-# chrome.get("https://www.pornhub.com")
-
-# Load of the page you cant access without cookies, this one will go through
-# chrome = webdriver.Chrome('./chromedriver')
-# load_cookies(chrome, cookies_location)
-# chrome.get("https://www.pornhub.com")
-# time.sleep(5)
-# chrome.quit()
-
-# chrome = webdriver.Chrome()
-# chrome.get("https://google.com")
-# time.sleep(2)
-# pprint.pprint(chrome.get_cookies())
-# print "=========================\n"
-#
-# delete_cookies(chrome, domains=["www.google.com"])
-# pprint.pprint(chrome.get_cookies())
-# print "=========================\n"
-#
-# delete_cookies(chrome)
-# pprint.pprint(chrome.get_cookies())
